651e30b3d2fbc3ecd11bbf142ed7b34369b95abb
Eva-Fong <evafong26750@gmail.com>
2026-01-27 19:24:11 -0500

    outliers

--- py_20260127.py
+++ py_20260127.py
@@ -61,3 +61,53 @@ sns.kdeplot(df['price'])
 sns.ecdfplot(df['price'])
 
 # %%
+#median is robust, meaning that changest to our highest/lowest values won't change it much, unlike mean
+
+#IQR (interquartile range) is the distance between the .25 and the .75 range aka 50% of the observation
+#Kinda like std. dev., essentially saying how spread out is the data based on its central tendencies
+
+x = df['price']
+
+#boxplot shows us which values are extreme and which arent!
+#anything outside of the whiskers are outliers
+sns.boxplot(x)
+
+# %%
+def outlier_analyze(x):
+    q75 =np.quantile(x, .75) 
+    q25 =np.quantile(x, .25) 
+
+    iqr = q75 - q25
+    upperWhisker = q75 + 1.5 * iqr
+    lowerWhisker = q25 - 1.5 * iqr
+
+    upperOutlier = ( x > upperWhisker).astype(int)
+
+    lowerOutlier = ( x < lowerWhisker).astype(int)
+    outlier = upperOutlier + lowerOutlier
+
+    #locates/displays the rows that are upperOutlier aka when the upperOutlier is true/1
+    df.loc[upperOutlier == 1, : ]
+
+    winsorize = {
+        upperOutlier * upperWhisker + #map upper outliers to upper whisker
+        
+        lowerOutlier * lowerWhisker + #map lower outliers to lower whisker
+        
+        (1 - outlier) * x #if neither, keep original value
+    }
+
+    return outlier, winsorize
+
+# %%
+outlier, winsorize = outlier_analyze(x)
+
+#now instead of the upper outlier going all the way up to 38,000, it has been mapped to ~20,000
+
+sns.scatterplot(x=x, y=winsorize)
+
+sns.boxplot(x = winsorize)
+
+# %%
+#the violin plot combines boxplot and kde plot
+sns.violinplot(x=x, fill=False, inner='quart')
